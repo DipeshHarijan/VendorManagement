714263b3ba444c976efe3e62305f577cb2ff6f10
DipeshHarijan <dipesh.harijan@cognizant.com>
2020-04-27 05:34:49 +0530

    initial commit

---
+++ VendorManagement/src/main/java/com/cts/repository/VendorRepository.java
@@ -0,0 +1,9 @@
+package com.cts.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.cts.entity.Vendor;
+
+public interface VendorRepository extends CrudRepository<Vendor, Long> {
+
+}
