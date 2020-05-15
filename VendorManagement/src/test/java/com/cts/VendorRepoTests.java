f430c98956a17aa5d36fb8400b93af450ddb8fc9
Jagruthi9 <60088124+Jagruthi9@users.noreply.github.com>
2020-05-15 11:08:50 +0530

    changes in repo testing

--- VendorManagement/src/test/java/com/cts/VendorRepoTests.java
+++ VendorManagement/src/test/java/com/cts/VendorRepoTests.java
@@ -1,7 +1,8 @@
 package com.cts;
 
-import static org.assertj.core.api.Assertions.linesOf;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -11,7 +12,7 @@ import java.util.Optional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -25,32 +26,33 @@ import com.cts.repository.VendorRepository;
 public class VendorRepoTests {
 	
 	
-//	private VendorService service;
-	
+
 	
 	@MockBean
 	private VendorRepository repo;
-	
-	
-	
+
 	@Test
 	public void findByTest() {
-		Vendor vendor=new Vendor(1L,"Devi Bomma","vizag","7995172182","devi@gmail.com");
-		when( repo.findById(1L)).thenReturn(Optional.of(vendor));
-				
-			
+		Vendor vendor=new Vendor(1L,"Name","city","7995174182","name@gmail.com");
+		when( repo.findById(1L)).thenReturn((vendor));
+		assertSame(1L, repo.findById(vendor.getVendorId()).getVendorId());
+
 	}
 	@SuppressWarnings("unchecked")
 	@Test
 	public void findallTest() {
+	@SuppressWarnings("rawtypes")
 	List vendor=new LinkedList();
-		 vendor.add(new Vendor(1L,"Devi Bomma","vizag","7995172182","devi@gmail.com"));
-		 vendor.add(new Vendor(2L,"satya Bomma","vizag","7995172142","satya@gmail.com"));
+	vendor.add(new Vendor(1,"Name","city","7995174182","name@gmail.com"));
+	vendor.add(new Vendor(2,"Name as","city as","7095174182","nameas@gmail.com"));
 		when( repo.findAll()).thenReturn(vendor);
-		List result=repo.findAll();
+		List result=(List) repo.findAll();
 		verify(repo).findAll();
-				
+
+	}
+	
 			
 	}
 
-}
+
+
